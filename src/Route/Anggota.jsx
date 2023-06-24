3346006225c659f2ac5a8a494be4027e2e8c4e37
AddienZakia <addienzakia1903@gmail.com>
2023-06-24 18:47:48 +0700

    menambahkan sesuatu

--- src/Route/Anggota.jsx
+++ src/Route/Anggota.jsx
@@ -1,18 +1,37 @@
+import axios from "axios";
+let link = "http://localhost:5500/kelas";
+
+async function Data() {
+  let { data } = await axios.get(link);
+
+  return (
+    <>
+      {data.map((res) => {
+        <p>www</p>;
+      })}
+    </>
+  );
+}
+
 function Anggota() {
   return (
     <table className="border-collapse border">
       <thead className="border ">
         <tr className="border">
-          <td className="border">hello</td>
-          <td className="border">hello</td>
+          <td className="border">Nama</td>
+          <td className="border">Agama</td>
+          <td className="border">Bahasa Indonesia</td>
+          <td className="border">Bahasa Inggris</td>
+          <td className="border">Biologi</td>
+          <td className="border">Ekonomi</td>
+          <td className="border">Fisika</td>
+          <td className="border">Geografi</td>
+          <td className="border">Kimia</td>
+          <td className="border">Matematika</td>
+          <td className="border">Sejarah</td>
         </tr>
       </thead>
-      <tbody className="border">
-        <tr className="border">
-          <td className="border">world</td>
-          <td className="border">world</td>
-        </tr>
-      </tbody>
+      <Data />
     </table>
   );
 }
